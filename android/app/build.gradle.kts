2571a46e48d5dae1b88cb40c575edcede478da45
daniel4191 <daniell8282@naver.com>
2025-11-18 15:38:00 +0900

    android bundle id changed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "daniell8282.class101.sticky_notes"
+        applicationId = "daniell8282.class101.stickynotes"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
