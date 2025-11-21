845f77d560e51848c90201387cec1cb0176397b9
daniel4191 <daniell8282@naver.com>
2025-11-21 15:25:16 +0900

    노트 색상 선택하여 추가

--- lib/page/note_edit_page.dart
+++ lib/page/note_edit_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../data/note.dart';
+
 class NoteEditPage extends StatefulWidget {
 
   @override
@@ -12,39 +14,103 @@ class _NoteEditPageState extends State<NoteEditPage>{
 
   final bodyController = TextEditingController();
 
+  Color color = Note.colorDefault;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("노트 편집"),
+        actions: [
+          IconButton(
+            onPressed: _displayColorSelectionDialog,
+            icon: Icon(Icons.color_lens),
+            tooltip: "배경색 선택",
+          ),
+        ],
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: "제목 입력",
-              ),
-              maxLines: 1,
-              style: TextStyle(fontSize: 20),
-              controller: titleController,
+      body: SizedBox.expand(
+        child: Container(
+          color: color,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "제목 입력",
+                  ),
+                  maxLines: 1,
+                  style: TextStyle(fontSize: 20),
+                  controller: titleController,
+                ),
+                SizedBox(height: 8),
+                TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "노트 입력",
+                  ),
+                  maxLines: null,
+                  keyboardType: TextInputType.multiline,
+                  controller: bodyController,
+                ),
+              ],
             ),
-            SizedBox(height: 8),
-            TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "노트 입력",
-              ),
-              maxLines: null,
-              keyboardType: TextInputType.multiline,
-              controller: bodyController,
-            ),
-          ],
+          ),
         ),
       ),
     );
+  }
+
+  void _displayColorSelectionDialog(){
+    FocusManager.instance.primaryFocus?.unfocus();
+    showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        title: Text("배경색 선택"),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              title: Text("없음"),
+              onTap: ()=> _applyColor(Note.colorDefault),
+            ),
+            ListTile(
+              leading: CircleAvatar(backgroundColor: Note.colorRed,),
+              title: Text("빨간색"),
+              onTap: ()=> _applyColor(Note.colorRed),
+            ),
+            ListTile(
+              leading: CircleAvatar(backgroundColor: Note.colorOrange,),
+              title: Text("오렌지색"),
+              onTap: ()=> _applyColor(Note.colorOrange),
+            ),
+            ListTile(
+              leading: CircleAvatar(backgroundColor: Note.colorYellow,),
+              title: Text("노란색"),
+              onTap: ()=> _applyColor(Note.colorYellow),
+            ),
+            ListTile(
+              leading: CircleAvatar(backgroundColor: Note.colorLime,),
+              title: Text("연두색"),
+              onTap: ()=> _applyColor(Note.colorLime),
+            ),
+            ListTile(
+              leading: CircleAvatar(backgroundColor: Note.colorBlue,),
+              title: Text("파란색"),
+              onTap: ()=> _applyColor(Note.colorBlue),
+            ),
+          ],
+        ),
+      );
+    });
+  }
+  
+  void _applyColor(Color newColor) {
+    setState(() {
+      Navigator.pop(context);
+      color = newColor;
+    });
   }
 }
